8f3b9c996ecd7b1bf7bb5f0d16c8e35a157ce80f
USER <arunnair9319@gmail.com>
2018-12-08 12:04:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');  
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    console.log(`${now} ${req.path} ${req.method}`);
+    var log = `${now} ${req.path} ${req.method}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log("Error:", err)
+        }
+    })
+    next();
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalise', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        currentYear: new Date().getFullYear(),
+        message: 'Welcome dude!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About page dynamic',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log("server is up at 3000");
+});
